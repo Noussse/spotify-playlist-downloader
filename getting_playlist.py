d2e8c27e5412f9ffee0810c1fd4dcdc32d099b1e
Noussse <149839925+Noussse@users.noreply.github.com>
2024-10-11 17:09:02 +0100

    added an option to download a chosen playlist not just the liked songs (still under development

--- getting_playlist.py
+++ getting_playlist.py
@@ -2,7 +2,7 @@ from requests import get, post
 from dotenv import load_dotenv
 import os
 import webbrowser
-from flask import Flask, request
+from flask import Flask, request,redirect
 load_dotenv()
 app = Flask(__name__)
 with open('liked_songs.txt', 'w') as f:
@@ -13,10 +13,22 @@ client_secret = os.getenv("client_secret")
 redirect_uri = 'http://localhost:5000/callback'
 scope = 'playlist-read-private user-library-read'
 auth_url = f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope={scope}"
-
+user_choice=None
 @app.route('/')
 def home():
-    return "Please authorize the application."
+     return """
+        <h1>Spotify Downloader</h1>
+        <h2>Please select an option:</h2>
+        <a href="/authorize?type=liked_songs">Download Liked Songs</a><br>
+        <a href="/authorize?type=saved_playlists">Download Saved Playlists</a>
+    """
+@app.route('/authorize')
+def authorize():
+    global user_choice
+    # Get the user's choice from the query parameter
+    user_choice = request.args.get('type')
+    # Redirect to Spotify for authorization
+    return redirect(auth_url)
 
 @app.route('/callback')
 def callback():
@@ -33,19 +45,26 @@ def callback():
     tokens = response.json()
     access_token=tokens.get('access_token')
     headers = {'Authorization': f'Bearer {access_token}'}
-    liked_songs_response = get('https://api.spotify.com/v1/me/tracks', headers=headers)
-    if liked_songs_response.status_code == 200:
-        liked_songs = liked_songs_response.json()
-        tracks = liked_songs['items']
-        with open('liked_songs.txt', 'w') as f:
-            for track in tracks:
-                f.write(f"{track['track']['name']} by {track['track']['artists'][0]['name']}\n")
-        return "Songs saved to (liked_songs.txt) and will start downloading soon You can close this window."
-    else :
-        return f"Failed to fetch liked songs.{liked_songs_response.status_code}{liked_songs_response.text}"
+    liked_songs_url='https://api.spotify.com/v1/me/tracks'
+    def register_songs(url):
+        songs_response = get(url, headers=headers)
+        if songs_response.status_code == 200:
+            songs = songs_response.json()
+            tracks = songs['items']
+            with open('liked_songs.txt', 'w') as f:
+                for track in tracks:
+                    f.write(f"{track['track']['name']} by {track['track']['artists'][0]['name']}\n")
+            return "Songs saved to (liked_songs.txt) and will start downloading soon You can close this window."
+        else :
+            return f"Failed to fetch liked songs.{songs_response.status_code}{songs_response.text}"
+    if user_choice=="liked_songs" :
+        result_message=register_songs(liked_songs_url)
+        return result_message
+    elif user_choice=="saved_playlists":
+        return "under devolpment"
 
 
 
 if __name__ == '__main__':
-    webbrowser.open(auth_url)
+    webbrowser.open('http://localhost:5000/')
     app.run(port=5000, debug=True)
